ff3c391e68c81b0cfd3166ad479d6b6b96e6b1e0
Perry Kivolowitz <perry@kivolowitz.com>
2017-04-01 21:28:30 +0000

    Logic problems fixed which caused buffer one to be hit twice the first time through the loop.
    Reduced the buffer size to a smaller multiple of 6 to ensure that
    ffmpeg can keep ahead of us.

--- fftest/main.cpp
+++ fftest/main.cpp
@@ -63,57 +63,13 @@ bool Play(string cmdline, unsigned char * buffers[], int buffer_size, const char
 		}
 		else
 		{
-			/* 	Scheme:
-
-				set flag to false
-				set buffer 0 as buffer_next
-					this means buffer 1 is buffer_prev
-
-				read into buffer_next (0);
-				while (bytes_read > 0)
-					if flag is true
-						while async read is not done
-							wait
-					toggle buffer_next
-					launch async read into buffer_next
-					launch write of buffer_prev to pulse
-					set flag to true
-
-				Pencil test:
-					flag is false
-					buffer 0 filled before loop with blocking read
-
-					flag is false so do not wait for async (there isn't one)
-					buffer 0 becomes buffer_prev and buffer 1 becomes buffer_next
-					non-blocking fill of buffer_next - buffer 1
-					blocking write buffer_prev to pulse - buffer 0
-						// write of buffer 0 is taking place while buffer 1 is filling
-					set flag true
-
-					loop around
-
-					flag is true so
-						wait until outstanding async read is done (this is buffer 1)	
-						it should be done by now anyway
-					buffer 1 becomes buffer_prev and buffer 0 becomes buffer_next
-					non-blocking fill of buffer_next - buffer 0
-					blocking write buffer_prev to pulse - buffer 1
-						// write of buffer 1 is taking place while buffer 0 is filling
-
-					loop around
-
-				Update - this ALMOST worked. The above scheme suffered a gap between 
-				the first and second blocks. To fix this, I am launching an async i/o
-				immediately after the first block's synchronous i/o completes.
-				The checking of "first time through the loop" is no longer needed.
-			*/
-
 			bool stop_flag = false;
 			int buffer_index = 0;
 			off_t read_offset = 0;
 			int fd = fileno(p);
 			aiocb cb;
 			
+			//cout << "block: 0 0" << endl;
 			bytes_read = fread(buffers[0], 1, buffer_size, p);
 			read_offset += bytes_read;
 
@@ -129,61 +85,69 @@ bool Play(string cmdline, unsigned char * buffers[], int buffer_size, const char
 			cb.aio_reqprio = 0;
 			cb.aio_sigevent.sigev_notify = SIGEV_NONE;
 			cb.aio_lio_opcode = LIO_NOP;
+			//cout << "async: 1 " << read_offset << " * " << endl;
 			error = aio_read(&cb);
+
+			bool first_loop = true;
 
 			while (bytes_read > 0 && stop_flag == false)
 			{
-				// When we get here the first time:
-				// 	buffer 0 is full
-				//  buffer 1 is in flight
-				// 	flag is true
-				//  buffer 0 is NextBuffer
-
-				// wait for async I/O - first time through this means waiting for buffer 1.
-				while ((error = aio_error(&cb)) == EINPROGRESS) 
-					usleep(100);
-				if (error > 0)
+				if (!first_loop)
 				{
-					perror("async i/o returned error");
-					rv = false;
-					break;
-				}
-				assert(error == 0);
-				int t = aio_return(&cb);
-				assert(t >= 0);
-				if (t == 0)
-					stop_flag = true;
-				read_offset += t;
-				bytes_read = t;
+					while ((error = aio_error(&cb)) == EINPROGRESS) 
+					{
+						usleep(100);
+					}
 
-				// first time through we have waited for buffer 1.
+					if (error > 0)
+					{
+						perror("async i/o returned error");
+						rv = false;
+						break;
+					}
+					assert(error == 0);
+					int t = aio_return(&cb);
+					assert(t >= 0);
+					if (t == 0)
+						stop_flag = true;
+					read_offset += t;
+					bytes_read = t;
+				}
+
 				if (bytes_read <= 0)
 					break;
 
 				BufferToggle(buffer_index);
 
-				// Launch async read into BufferNext()
-				memset(&cb, 0, sizeof(aiocb));
-				cb.aio_fildes = fd;
-				cb.aio_offset = read_offset;
-				cb.aio_buf = (void *) buffers[BufferNext(buffer_index)];
-				cb.aio_nbytes = buffer_size;
-				cb.aio_reqprio = 0;
-				cb.aio_sigevent.sigev_notify = SIGEV_NONE;
-				cb.aio_lio_opcode = LIO_NOP;
-				error = aio_read(&cb);
+				if (!first_loop)
+				{
+					memset(&cb, 0, sizeof(aiocb));
+					cb.aio_fildes = fd;
+					cb.aio_offset = read_offset;
+					cb.aio_buf = (void *) buffers[BufferNext(buffer_index)];
+					cb.aio_nbytes = buffer_size;
+					cb.aio_reqprio = 0;
+					cb.aio_sigevent.sigev_notify = SIGEV_NONE;
+					cb.aio_lio_opcode = LIO_NOP;
+					//cout << "async: " << BufferNext(buffer_index) << " " << read_offset << endl;
+					error = aio_read(&cb);
+	
+					assert(error >= 0);
+					assert(bytes_read > 0);
+				}
 
-				assert(error >= 0);
-				assert(bytes_read > 0);
+				first_loop = false;
 
 				// Launch blocking write to pulse
 				pulse_error = 0;
 				bytes_read = bytes_read / 6 * 6;
+				//cout << "rendr: " << BufferPrev(buffer_index) << " " << bytes_read << endl;
 				if (pa_simple_write(pas, (const void *) buffers[BufferPrev(buffer_index)], bytes_read, &pulse_error) < 0)
 				{
 					cerr << "lost my pulse: " << pa_strerror(pulse_error) << endl;
 					break;
 				}
+				//cout << endl;
 			}
 		}
 		if (pas != nullptr)
@@ -207,7 +171,7 @@ int main(int argc, char * argv[])
 	unsigned char * buffer_2 = nullptr;
 
 	// The div / mult by 6 is essential.
-	int BUFFER_SIZE = ((1 << 20) * (1 << 0)) / 6 * 6;
+	int BUFFER_SIZE = (1 << 12) * 6; //((1 << 20) * (1 << 0)) / 6 * 6;
 	string cmdline;
 
 	if (argc < 2)
