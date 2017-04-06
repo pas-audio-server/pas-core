33aac45e31278bdbf2c8641b4d55d7ff5b6206e2
Perry Kivolowitz <perry@kivolowitz.com>
2017-04-06 04:56:24 +0000

    Added support for duration and several other tags

--- server/track.cpp
+++ server/track.cpp
@@ -18,6 +18,12 @@ void Track::SetPath(string & path)
 
 	After parsing the tag, the key-value pair is added to the
 	track's tag map.
+
+		// adding -show_entries format=duration -sexagesimal
+		// will get the duration but it isn't in the same format.
+		// instead it looks like this:
+		// duration=0:04:19.213061
+		// Luckily, the mysql TIME type will eat this directly.
 */
 void Track::SetTag(string & raw)
 {
@@ -25,7 +31,13 @@ void Track::SetTag(string & raw)
 	regex t(":.*=");
 	regex v("=.*\n");
 
-	if (regex_search(raw, c))
+	if ((raw.size() > 12) && (raw.substr(0, 8) == "duration"))
+	{
+		string t = "duration";
+		string v = raw.substr(9, string::npos);
+		SetTag(t, v);
+	}
+	else if (regex_search(raw, c))
 	{
 		smatch mtag;
 		smatch mval;
