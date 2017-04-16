df3d103cd11214eda06806519138df0b299c1c81
LOZORD <ljr@google.com>
2017-04-15 20:00:43 -0400

    Updated pashttp to read from pas correctly.
    Also added a new test handler that roundtrips (w+r) pas interaction.

--- intermediaries/http_intermediary/pashttp/pashttp.go
+++ intermediaries/http_intermediary/pashttp/pashttp.go
@@ -3,7 +3,6 @@ package pashttp
 import (
 	"encoding/binary"
 	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
 
@@ -35,6 +34,7 @@ func NewPasHTTPIntermediary(port uint64) (*HTTPIntermediary, error) {
 }
 
 func (i *HTTPIntermediary) setUpRouter() {
+	/* TESTING ROUTES */
 	i.Router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world!"))
 	})
@@ -42,6 +42,27 @@ func (i *HTTPIntermediary) setUpRouter() {
 	i.Router.HandleFunc("/aloha", func(w http.ResponseWriter, r *http.Request) {
 		i.WriteToPas([]byte("what is up my dude"))
 	})
+
+	i.Router.HandleFunc("/wazzup", func(w http.ResponseWriter, r *http.Request) {
+		buff := make([]byte, 1024)
+
+		// This makes it easier to view the response in the browser.
+		w.Header().Set("Content-Type", "text/html")
+
+		if err := i.WriteToPas([]byte("Hello pas!")); err != nil {
+			w.Write([]byte(fmt.Sprintf("write error: %v", err)))
+			return
+		}
+
+		_, err := i.ReadFromPas(buff)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("read error: %v", err)))
+			return
+		}
+
+		w.Write(buff)
+	})
+	/* END TESTING ROUTES */
 }
 
 // WriteToPas sends the given data to the pas server.
@@ -55,6 +76,7 @@ func (i *HTTPIntermediary) WriteToPas(data []byte) error {
 		return fmt.Errorf("WriteToPas: message is too long to encode")
 	}
 
+	// TODO(lozord): Test the length-encoding prefix.
 	payload := append(dl, data...)
 	wroteLen, err := i.PasConn.Write(payload)
 	if err != nil {
@@ -74,8 +96,8 @@ func (i *HTTPIntermediary) WriteToPas(data []byte) error {
 }
 
 // ReadFromPas reads the most recent data available from the pas server.
-func (i *HTTPIntermediary) ReadFromPas() ([]byte, error) {
-	return ioutil.ReadAll(i.PasConn)
+func (i *HTTPIntermediary) ReadFromPas(buff []byte) (int, error) {
+	return i.PasConn.Read(buff)
 }
 
 // Shutdown contains the destruction logic for an HTTPIntermediary.
