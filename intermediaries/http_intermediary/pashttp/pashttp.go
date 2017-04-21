647d0856789e661a4e8ce5c15121191d09b7d713
LOZORD <ljr@google.com>
2017-04-20 22:24:17 -0400

    Updated pashttp and tests.
    I still need to fix a few things and implement proper TCP framing.

--- intermediaries/http_intermediary/pashttp/pashttp.go
+++ intermediaries/http_intermediary/pashttp/pashttp.go
@@ -9,11 +9,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const debug = false
+const (
+	debug                = false
+	BUFF_SIZE            = 1024
+	BYTES_IN_DATA_LENGTH = 4
+)
+
+// TODO(lozord): Make a PasBuffer type for easier serialization.
 
 // HTTPIntermediary handles client-to-pas communication via HTTP.
 type HTTPIntermediary struct {
 	Router  *mux.Router
+	// We keep the connection alive until it is forcibly closed or errors.
 	PasConn net.Conn
 }
 
@@ -54,7 +61,7 @@ func (i *HTTPIntermediary) setUpRouter() {
 			return
 		}
 
-		_, err := i.ReadFromPas(buff)
+		buff, err := i.ReadFromPas()
 		if err != nil {
 			w.Write([]byte(fmt.Sprintf("read error: %v", err)))
 			return
@@ -70,14 +77,15 @@ func (i *HTTPIntermediary) WriteToPas(data []byte) error {
 	var wroteLen int
 
 	// Prepend the length of the message.
-	dl := make([]byte, 8) // 64 = 8 * 8
-	wroteLen = binary.PutVarint(dl, int64(len(data)))
+	dataLenBuff := make([]byte, BYTES_IN_DATA_LENGTH)
+	wroteLen = binary.PutVarint(dataLenBuff, int64(len(data)))
 	if wroteLen < 0 {
 		return fmt.Errorf("WriteToPas: message is too long to encode")
 	}
 
 	// TODO(lozord): Test the length-encoding prefix.
-	payload := append(dl, data...)
+	// TODO(lozord): Separately send the length and data (see wiki).
+	payload := append(dataLenBuff, data...)
 	wroteLen, err := i.PasConn.Write(payload)
 	if err != nil {
 		return err
@@ -96,8 +104,35 @@ func (i *HTTPIntermediary) WriteToPas(data []byte) error {
 }
 
 // ReadFromPas reads the most recent data available from the pas server.
-func (i *HTTPIntermediary) ReadFromPas(buff []byte) (int, error) {
-	return i.PasConn.Read(buff)
+func (i *HTTPIntermediary) ReadFromPas() ([]byte, error) {
+	buff := make([]byte, BUFF_SIZE)
+	// TODO(lozord): Need to correctly implement frame handling (see wiki).
+	readLen, err := i.PasConn.Read(buff)
+	if err != nil {
+		return nil, err
+	}
+
+	if readLen < BYTES_IN_DATA_LENGTH {
+		return nil, fmt.Errorf(
+			"read only %d bytes from pas, wanted at least %d",
+			readLen, BYTES_IN_DATA_LENGTH,
+		)
+	}
+
+	dataLen, bytesRead := binary.Varint(buff[:BYTES_IN_DATA_LENGTH])
+	if bytesRead < BYTES_IN_DATA_LENGTH {
+		return nil, fmt.Errorf("could not read data length from the payload")
+	}
+
+	// Sanity check.
+	if expectedLen := int(BYTES_IN_DATA_LENGTH + dataLen); readLen != expectedLen {
+		return nil, fmt.Errorf(
+			"read %d bytes from pas, but given (size + data) = %d was not equal",
+			readLen, expectedLen,
+		)
+	}
+
+	return buff[BYTES_IN_DATA_LENGTH:], nil
 }
 
 // Shutdown contains the destruction logic for an HTTPIntermediary.
--- intermediaries/http_intermediary/pashttp/pashttp_test.go
+++ intermediaries/http_intermediary/pashttp/pashttp_test.go
@@ -1,7 +1,111 @@
 package pashttp
 
-import "testing"
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+)
 
-func TestStub(t *testing.T) {
-	t.Errorf("This should be tested!")
+type fakeAddr struct {
+	n string
+	s string
+}
+
+func (f fakeAddr) Network() string { return f.n }
+func (f fakeAddr) String() string  { return f.s }
+
+type fakePasConn struct {
+	received [][]byte
+	toSend   [][]byte
+	closed   bool
+}
+
+func (f *fakePasConn) Read(b []byte) (int, error) {
+	if f.closed {
+		return 0, fmt.Errorf("connection closed")
+	}
+
+	if len(f.toSend) < 1 {
+		return 0, fmt.Errorf("no data to send!")
+	}
+
+	msg := f.toSend[0]
+
+	if len(b) < len(msg) {
+		return len(msg) - len(b), fmt.Errorf("argument buffer is too short!")
+	}
+
+	f.toSend = f.toSend[1:]
+
+	for i := range msg {
+		b[i] = msg[i]
+	}
+
+	return len(msg), nil
+}
+
+func (f *fakePasConn) Write(b []byte) (int, error) {
+	if f.closed {
+		return 0, fmt.Errorf("connection closed")
+	}
+
+	f.received = append(f.received, b)
+	return len(b), nil
+}
+
+func (f *fakePasConn) Close() error {
+	if f.closed {
+		return fmt.Errorf("connection already closed")
+	}
+
+	f.closed = true
+	return nil
+}
+
+func (f fakePasConn) LocalAddr() net.Addr                { return fakeAddr{} }
+func (f fakePasConn) RemoteAddr() net.Addr               { return fakeAddr{} }
+func (f fakePasConn) SetDeadline(t time.Time) error      { return nil }
+func (f fakePasConn) SetReadDeadline(t time.Time) error  { return nil }
+func (f fakePasConn) SetWriteDeadline(t time.Time) error { return nil }
+
+func NewFakePasConn() *fakePasConn {
+	return &fakePasConn{
+		received: [][]byte{},
+		toSend:   [][]byte{},
+		closed:   false,
+	}
+}
+
+func TestWriteToPasSuccess(t *testing.T) {
+	const msg = "reticulating-splines"
+	buffToWrite := []byte(msg)
+	pc := NewFakePasConn()
+	hi := HTTPIntermediary{PasConn: pc}
+	if err := hi.WriteToPas(buffToWrite); err != nil {
+		t.Errorf("WriteToPas(%v) got error %v, want no error", buffToWrite, err)
+		return
+	}
+
+	if len(pc.received) != 1 {
+		t.Errorf("WriteToPas(%v) got %d messages received, wanted 1", buffToWrite, len(pc.received))
+		return
+	}
+
+	pl := pc.received[0]
+	dataLenBuff := pl[:BYTES_IN_DATA_LENGTH]
+	data := pl[BYTES_IN_DATA_LENGTH:]
+	if string(data) != msg {
+		t.Errorf("WriteToPas(%v) got payload data %q, want %q", buffToWrite, string(data), msg)
+	}
+
+	gotDataLen, bytesRead := binary.Varint(dataLenBuff)
+	if bytesRead <= 0 {
+		t.Errorf("WriteToPas(%v) got %d bytes read, want %d", buffToWrite, bytesRead, BYTES_IN_DATA_LENGTH)
+	}
+
+	if int(gotDataLen) != len(buffToWrite) {
+		t.Errorf("WriteToPas(%v) got payload length of %d, want %d", buffToWrite, gotDataLen, len(buffToWrite))
+	}
 }
